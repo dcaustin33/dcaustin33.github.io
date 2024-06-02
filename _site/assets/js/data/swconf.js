08e220fa81ac031f5806aa2c138bb75ef543cddb
dcaustin33 <dcaustin33@gmail.com>
2024-06-02 13:56:38 -0400

    Change base url

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717349606',resources: [
+    cacheName: 'chirpy-1717350933',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
