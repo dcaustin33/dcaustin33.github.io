72379785ab318c196fd03d02914cae89c07101a9
dcaustin33 <dcaustin33@gmail.com>
2024-06-02 14:15:55 -0400

    change ruby v

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717351358',resources: [
+    cacheName: 'chirpy-1717352142',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
