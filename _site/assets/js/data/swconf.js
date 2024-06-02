89bc93ab0cc6bac19cbfed8ca64ee785d963fe00
dcaustin33 <dcaustin33@gmail.com>
2024-06-02 13:53:09 -0400

    update url

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,22 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1717349606',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/',
+      
+        '/posts/',
+      
+        '/posts_and_publications/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
