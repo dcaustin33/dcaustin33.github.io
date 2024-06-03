6672a5c14a06a0e3d39e2b24e2c2bd4a6ca43584
dcaustin33 <dcaustin33@gmail.com>
2024-06-03 12:25:06 -0400

    most recent changes

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717352431',resources: [
+    cacheName: 'chirpy-1717352692',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
