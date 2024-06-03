09c8dcf29e46ba8db966901f4b69830823440702
dcaustin33 <dcaustin33@gmail.com>
2024-06-03 12:34:21 -0400

    remove if

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717352692',resources: [
+    cacheName: 'chirpy-1717432447',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
